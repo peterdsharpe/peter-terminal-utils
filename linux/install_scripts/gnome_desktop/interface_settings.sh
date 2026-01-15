23489655eb0400dcaaf4ea9447ff744022cdb181
Peter Sharpe <peterdsharpe@gmail.com>
2026-01-15 14:09:21 -0500

    Enhance GNOME interface settings script to include font configuration options. Updated description to reflect new settings for monospace font, font hinting, and antialiasing. Added dependency annotation for FiraCode font.

--- linux/install_scripts/gnome_desktop/interface_settings.sh
+++ linux/install_scripts/gnome_desktop/interface_settings.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # @name: Interface Settings
-# @description: Theme, animations, clock, and display preferences
+# @description: Theme, animations, clock, fonts, and display preferences
+# @depends: firacode.sh
 # @headless: skip
 source "$(dirname "${BASH_SOURCE[0]}")/../../_common.sh"
 standalone_init
@@ -11,6 +12,11 @@ skip_if_not_gnome "Interface settings"
 ### Appearance
 step "Setting dark theme" gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 step "Disabling GNOME animations" gsettings set org.gnome.desktop.interface enable-animations false
+
+### Font rendering (GNOME Tweaks equivalent settings)
+step "Setting monospace font to FiraCode Nerd Font" gsettings set org.gnome.desktop.interface monospace-font-name 'FiraCode Nerd Font Mono 11'
+step "Setting font hinting to full" gsettings set org.gnome.desktop.interface font-hinting 'full'
+step "Setting font antialiasing to subpixel (LCD)" gsettings set org.gnome.desktop.interface font-antialiasing 'rgba'
 
 ### Accessibility / usability
 step "Disabling hot corners" gsettings set org.gnome.desktop.interface enable-hot-corners false
